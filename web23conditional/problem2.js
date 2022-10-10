b2974d902fe4ff0498b2851282eab488134a52c3
abhiramkumars <112821859+abhiramkumars@users.noreply.github.com>
2022-10-10 21:50:39 +0530

    day 2

---
+++ web23conditional/problem2.js
@@ -0,0 +1,9 @@
+
+
+let age=21
+
+if(age>18){
+  console.log("NA")
+}else{
+  console.log("Apply for a license")
+}
---
+++ web23conditional/problem4.js
@@ -0,0 +1,13 @@
+
+
+user_name="Abhi"
+pass="Bhai"
+
+needed_name="Abhi"
+needed_pass="Bhai"
+
+if(user_name==needed_name&&pass==needed_pass){
+  console.log("login successful")
+}else{
+  console.log("failed")
+}
